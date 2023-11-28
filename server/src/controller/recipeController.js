aea5d77efbcbde2a1ae3afe898146ee46c3ef1d2
vlad.pliska <35779767+VladPliska@users.noreply.github.com>
2023-11-28 10:39:37 +0200

    review recipeController

--- server/src/controller/recipeController.js
+++ server/src/controller/recipeController.js
@@ -1,32 +1,43 @@
-const express=require("express");
+const express=require("express");     // COMMENT: add tabulation like in lines 3-4
 const mongoose=require("mongoose");
-const bigPromise = require("../middlewares/bigPromise.js");
+const bigPromise = require("../middlewares/bigPromise.js");        // COMMENT: Chande to understand the name, use another way to handle the exception. Example: Global Exception Filter
 const CustomError = require("../utils/customErrors.js");
 
 const recipesModel=require("../models/recipeModel");
 const UserModel=require("../models/userModel");
 
+/** 
+    !! GENERAL COMMENTS !!
 
+    COMMENT: Use the module as an object and then send the current module to `module.exports = { ...module }`
+        Also, I recommend using `inversify` for creating IoC container for other dependencies
 
+    COMMENT: setup `eslint` for checking the correct code style
+    
+    COMMENT: Also here we need a layer for validation we can use `class-validator` or `joi`.
+    COMMENT: remove this line to separate the code block
+**/
 // //----------------------------------------------------------------------------------
 
-exports.getAllRecipes=bigPromise(async(req,res,next)=>{
-    const result=await recipesModel.find({});
+exports.getAllRecipes=bigPromise(async(req,res,next)=>{             // COMMENT: add space between variables and names  
+    const result=await recipesModel.find({});                       // COMMENT: move this logic to service layer
     res.status(200).json(result);
 })
 
 
 // //------------------------------------------------------------------------------------
 
+// COMMENT: How front-end team will know which data needs to be sent for this end-point? Answer: Use OpenAPI to document your API.
+// COMMENT: Are all users can add recipes? Provide auth and permission.
 exports.createRecipe=bigPromise(async(req,res,next)=>{
     const {name,ingredients,instructions,imageUrl,cookingTime} =req.body;
     const userObjID=req.user._id;
     const userOwner=userObjID.toString();
 
-    //console.log("user ki id: ",userOwner);
+    //console.log("user ki id: ",userOwner);        // COMMENT: remove comment
 
-    if(!userOwner){
-        return next(new CustomError("Please provide user id",400));
+    if(!userOwner){                                    
+        return next(new CustomError("Please provide user id",400));              // COMMENT: use the validation layer for validation errors.
     }
     if(!name || !ingredients || !instructions || !cookingTime ){
         return next(new CustomError("Please provide all the fields",400));
@@ -43,11 +54,26 @@ exports.createRecipe=bigPromise(async(req,res,next)=>{
             cookingTime:cookingTime,
             userOwner:userOwner
         }   
+                                                    /**
+                                                        COMMENT: replace object to 
+                                                           {
+                                                            _id: new mongoose.Types.ObjectId(),
+                                                            name,
+                                                            ingredients,
+                                                            instructions,
+                                                            imageUrl,
+                                                            cookingTime,
+                                                            userOwner,
+                                                        }   
+                                                    **/
+        
     );
-    console.log("Recipe is here:", recipe);
+    console.log("Recipe is here:", recipe);         // COMMENT: remove console.log(), I recommend using a special tool for logging: `morgan`, `pino `
     
     //save created recipe in DB
     const result=await recipe.save();
+
+    //COMMENT: use 1 way to send a response.
     res.status(201).json({
         createdRecipe: {
             name: result.name,
@@ -64,7 +90,7 @@ exports.createRecipe=bigPromise(async(req,res,next)=>{
 
 // //------------------------------------------------------------------------------------
 
-
+// COMMENT: The same comments as for the previous end-point.
 exports.getRecipeById=bigPromise(async(req,res,next)=>{
     const{recipeId} =req.params;
     if(!recipeId){
@@ -80,7 +106,7 @@ exports.getRecipeById=bigPromise(async(req,res,next)=>{
 
 // //------------------------------------------------------------------------------------
 
-
+// COMMENT: The same comments as for the previous end-point.
 exports.saveRecipes=bigPromise(async(req,res,next)=>{
     const {recipeId,userId}=req.body;
     if(!recipeId || !userId){
@@ -103,7 +129,7 @@ exports.saveRecipes=bigPromise(async(req,res,next)=>{
 // //------------------------------------------------------------------------------------
 
 
-
+// COMMENT: The same comments as for the previous end-point.
 exports.getSavedRecipeById=bigPromise(async(req,res,next)=>{
     const {userId}=req.params;
     if(!userId){
@@ -121,7 +147,7 @@ exports.getSavedRecipeById=bigPromise(async(req,res,next)=>{
 
 // //------------------------------------------------------------------------------------
 
-
+// COMMENT: The same comments as for the previous end-point.
 exports.getSavedRecipes=bigPromise(async(req,res,next)=>{
     const {userId}=req.params;
     if(!userId){
@@ -141,7 +167,7 @@ exports.getSavedRecipes=bigPromise(async(req,res,next)=>{
 
 // //------------------------------------------------------------------------------------
 
-
+// COMMENT: The same comments as for the previous end-point.
 exports.deleteRecipe=bigPromise(async(req,res,next)=>{
     const {recipeId}=req.params;
     if(!recipeId){
@@ -157,7 +183,7 @@ exports.deleteRecipe=bigPromise(async(req,res,next)=>{
 
 
 // //------------------------------------------------------------------------------------
-
+// COMMENT: The same comments as for the previous end-point.
 exports.updateRecipe=bigPromise(async(req,res,next)=>{
     const {recipeId}=req.params;
     const {name,ingredients,instructions,imageUrl,cookingTime}=req.body;
